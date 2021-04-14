fcf8943619c6b438e1e8acf54ecdc1bd3b5859c3
MrBBBaiXue <wch272@outlook.com>
2021-04-14 22:11:58 +0800

    # 10

--- scripts/Arena.gd
+++ scripts/Arena.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var enemy_1 = preload("res://scenes/Enemy.tscn")
+export(Array, PackedScene) var enemies
 
 func _ready():
 	Global.node_creation_parent = self
@@ -17,11 +17,15 @@ func _on_EnemySpawnTimer_timeout():
 	while enemy_position.x < 640 and enemy_position.x > -80 and enemy_position.y < 360 and enemy_position.y  > -45:
 		enemy_position = Vector2(rand_range(-160,670), rand_range(-90,390))
 	
-	Global.instance_node(enemy_1, enemy_position, self)
+	# 0-2 
+	# random a index to generate.
+	# ToDo : 其实应该增加生成权重
+	var enemy_index = randi() % enemies.size()
+	Global.instance_node(enemies[enemy_index], enemy_position, self)
 	pass
 
 
 func _on_DifficultyTimer_timeout():
 	if $EnemySpawnTimer.wait_time > 0.5:
-		$EnemySpawnTimer.wait_time -= 0.1
+		$EnemySpawnTimer.wait_time -= 0.05
 	pass
--- scripts/EnemyCore.gd
+++ scripts/EnemyCore.gd
@@ -1,20 +1,25 @@
 extends Sprite
 
-var speed = 75
+export(int) var speed = 75
+export(int) var hp = 3
+export(int) var knock_back = 600
+export(int) var screen_shake = 120
+export(int) var score = 10
 
 var velocity = Vector2()
-
 var stun = false
-var hp = 3
 
 var blood_particles = preload("res://scenes/BloodParticles.tscn")
+
+# onready 代表 初始化完成之后再声明变量
+onready var current_color = modulate
 
 func _process(_delta):
 	if hp <= 0:
 		if Global.camera != null:
-			Global.camera.screen_shake(180, 0.3)
+			Global.camera.screen_shake(screen_shake, 0.3)
 		# add score
-		Global.score += 10
+		Global.score += score
 		# instance node
 		if Global.node_creation_parent != null:
 			var blood_particles_instance = Global.instance_node(blood_particles, global_position, Global.node_creation_parent)
@@ -26,17 +31,17 @@ func basic_movement(delta):
 	if Global.player != null and !stun:
 		# 表明Player存在
 		velocity = global_position.direction_to(Global.player.global_position)
+		global_position += velocity * speed * delta
 	elif stun:
 		velocity = lerp(velocity, Vector2(0,0), 0.3)
-		
-	global_position += velocity * speed * delta
-	pass
+		global_position += velocity * delta
+	pass	
 
 func _on_HitBox_area_entered(area):
 	# 注意 stun == false, 防止在击退状态下再次“击退”
 	if area.is_in_group("enemy_damager") and !stun:
 		modulate = Color.white
-		velocity = - velocity * 6
+		velocity = - velocity * knock_back
 		# 击中震动
 		if hp > 1 :
 			Global.camera.screen_shake(10, 0.05)
@@ -47,6 +52,6 @@ func _on_HitBox_area_entered(area):
 pass
 
 func _on_StunTimer_timeout():
-	modulate = Color("ff414e")
+	modulate = current_color
 	stun = false
 	pass
